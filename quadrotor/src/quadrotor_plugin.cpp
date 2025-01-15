7b7a50ceaec5ace37251a7b0639fc1b195ddcd25
Cody <codyuehara@gmail.com>
2025-01-14 22:11:38 -0800

    Added dockerfile and camera for quadrotor in gazebo

--- quadrotor/src/quadrotor_plugin.cpp
+++ quadrotor/src/quadrotor_plugin.cpp
@@ -11,9 +11,13 @@ class QuadrotorPluginPrivate
 public:
     gazebo::event::ConnectionPtr update_connection_;
     gazebo_ros::Node::SharedPtr ros_node_;
-    gazebo::physics::JointPtr joint;
+    gazebo::physics::JointPtr belt_joint_;
 
-    double max_velocity;
+    double limit_;
+
+    double max_velocity_;
+    
+    void OnUpdate();
 
 };
 
@@ -30,6 +34,35 @@ void QuadrotorPlugin::Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf)
 {
     impl_->ros_node_ = gazebo_ros::Node::Get(sdf);
     RCLCPP_INFO_ONCE(impl_->ros_node_->get_logger(), "CODY");
+
+//    impl_->belt_joint_ = model->GetJoint("belt_joint");
+
+    //if (!impl_belt_joint_) {
+     //   RCLCPP_ERROR(impl_->ros_node_->get_logger(), "Joint not found, unable to start plugin");
+//	return;
+ //   }
+
+    impl_->max_velocity_ = sdf->GetElement("max_velocity")->Get<double>();
+
+    //model->GetLink("base_link")->SetLinearVel({0, 1, 0});
+
+    impl_->update_connection_ = gazebo::event::Events::ConnectWorldUpdateBegin(std::bind(&QuadrotorPluginPrivate::OnUpdate, impl_.get()));
+}
+
+void QuadrotorPluginPrivate::OnUpdate()
+{
+    //printf("OnUpdate called \n");
+
+    // a = g + 1/m R(q)f_T
+    // q_dot = 1/2q o [0 w]^T
+    //
+    // x_dot = [v_x, v_y, v_z, a_x, a_y, a_z, q_w, q_x, q_y, q_z, w_x, w_y, w_z] 
+    //
+    // integrate with runge kutta 4 to get:
+    // states = [x, y, z, v_x, v_y, v_z, p, q, r, w_x, w_y, w_z]
+    //
+    // model->GetLink("link")->SetLinearVel({v_x, v_y, v_x});
+    // model->GetLink("link")->SetAngularVel({w_x, w_y, w_x});
 }
 
 GZ_REGISTER_MODEL_PLUGIN(QuadrotorPlugin)
